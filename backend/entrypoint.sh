baa7086824260c2302dfcc6f33cdec9b3b200ede
Emiliopg91 <ojosdeserbio@gmail.com>
2025-10-29 20:59:25 +0100

    Backend remove

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,21 +3,4 @@ set -e
 
 echo "Generating backend"
 
-echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
-
-cd /backend
-
-echo "--- Building ryzenadj lib ---"
-git clone https://github.com/FlyGoat/RyzenAdj /tmp/ryzenadj
-cd /tmp/ryzenadj
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. || exit 1
-make || exit 1
-mv libryzenadj.so /backend/out/libryzenadj.so || exit 1
-mv ryzenadj /backend/out/ryzenadj || exit 1
-chmod +x /backend/out/ryzenadj || exit 1
-mv ../LICENSE /backend/out/LICENSE-ryzenadj || exit 1
-
-cd /backend
-
 echo "Generated backend"
