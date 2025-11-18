e7180047d4c0a1e53045872f8005a2f257ba2447
Emiliopg91 <ojosdeserbio@gmail.com>
2025-11-18 17:57:19 +0100

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+# credit: https://github.com/totallynotbakadestroyer/Decky-Undervolt/blob/master/backend/entrypoint.sh
+
 set -e
 
-echo "Generating backend"
+echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 
 cd /backend
 
@@ -10,14 +13,14 @@ git clone https://github.com/FlyGoat/RyzenAdj /tmp/ryzenadj
 cd /tmp/ryzenadj
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. || exit 1
-tree
 make || exit 1
+mv libryzenadj.so /backend/out/libryzenadj.so || exit 1
 mv ryzenadj /backend/out/ryzenadj || exit 1
 chmod +x /backend/out/ryzenadj || exit 1
+mv ../LICENSE /backend/out/LICENSE-ryzenadj || exit 1
 
 cd /backend
 
 echo "--- Cleaning up ---"
+# remove newly-cloned git repo and artifacts
 rm -rf ./ryzenadj
-
-echo "Generated backend"
