d583b78f3c3e57ef7cfb143f7c08320bf763e876
Emiliopg91 <ojosdeserbio@gmail.com>
2025-11-19 00:32:17 +0100

    Remove boot entry

--- src/components/system/main.tsx
+++ src/components/system/main.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react';
 
 import { BiosBlock } from './biosBlock';
-import { BootBlock } from './bootBlock';
 import { PluginBlock } from './pluginBlock';
 
 export const SystemBlock: FC = () => {
   return (
     <>
-      <BootBlock />
       <BiosBlock />
       <PluginBlock />
     </>
