90bedf3a02588555322a151c0188f54141300030
Emiliopg91 <ojosdeserbio@gmail.com>
2025-11-18 17:59:58 +0100

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -14,7 +14,7 @@ cd /tmp/ryzenadj
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. || exit 1
 make || exit 1
-mv libryzenadj.so /backend/out/libryzenadj.so || exit 1
+mkdir -p /backend/out
 mv ryzenadj /backend/out/ryzenadj || exit 1
 chmod +x /backend/out/ryzenadj || exit 1
 mv ../LICENSE /backend/out/LICENSE-ryzenadj || exit 1
